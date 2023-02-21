54c9c57ff529117fc6f27e5d9020cc2e73f2b0e6
MaxHearnden <MaxHearnden@users.noreply.github.com>
2023-02-21 19:10:30 +0000

    add semicolon

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
   outputs = { nixpkgs, self, ... }: {
-    packages = nixpkgs.lib.mapAttrs (_: pkgs: import ./default.nix {inherit pkgs}) nixpkgs.legacyPackages;
+    packages = nixpkgs.lib.mapAttrs (_: pkgs: import ./default.nix {inherit pkgs;}) nixpkgs.legacyPackages;
  };
 }
