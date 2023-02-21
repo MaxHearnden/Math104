615ab409dd5f481dccfecb88fc865326621a752c
MaxHearnden <MaxHearnden@users.noreply.github.com>
2023-02-21 11:42:52 +0000

    name -> pname

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> {} }: rec {
   default = pkgs.rPackages.buildRPackage {
-    name = "Math104";
+    pname = "Math104";
     version = "1.0";
     src = ./Math104_1.0.tar.gz;
     buildInputs = with pkgs.rPackages; [maps mapproj pkgs.R];
