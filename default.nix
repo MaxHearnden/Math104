9bfdc49afce1bff5c0ede959fae4c4123a5289ba
MaxHearnden <maxoscarhearnden@gmail.com>
2023-02-23 21:51:40 +0000

    revert packaging

--- default.nix
+++ default.nix
@@ -3,13 +3,12 @@
     name = "Math104";
     version = "1.0";
     src = ./Math104_1.0.tar.gz;
-    propogatedBuildInputs = with pkgs.rPackages; [maps mapproj];
-    nativeBuildInputs = with pkgs.rPackages; [maps mapproj pkgs.R];
+    buildInputs = with pkgs.rPackages; [maps mapproj pkgs.R];
   };
   rEnv = pkgs.rWrapper.override {
-    packages = [ default ];
+    packages = [ default maps mapproj ];
   };
   rstudioEnv = pkgs.rstudioWrapper.override {
-    packages = [ default ];
+    packages = [ default maps mapproj ];
   };
 }
