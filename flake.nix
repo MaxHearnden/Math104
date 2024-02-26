499c82492a9bf1e698a71e1cdf843aac587ae259
MaxHearnden <maxoscarhearnden@gmail.com>
2024-02-26 16:05:46 +0000

    add nixpkgs-unstable as an explicit dependency

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
   outputs = { nixpkgs, self, ... }: {
     packages = nixpkgs.lib.mapAttrs (_: pkgs: import ./default.nix {inherit pkgs;}) nixpkgs.legacyPackages;
  };
